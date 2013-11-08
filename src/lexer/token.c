77a74cfcdff8b9951dd971204844aefc7132bcdb
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-08 14:17:42 +0100

    lexer: print token (debug)

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -3,15 +3,17 @@
 
 void token_print(s_token *tok)
 {
-#define X(Type, Str) \
-    if (tok->type == Type) \
-    { \
-        fprintf(stdout, "%s", ##Type);\
-        break;\
+#define X(Type, Str)                            \
+    if (tok->type == Type)                      \
+    {                                           \
+        fprintf(stdout, "%s", ##Type);          \
+        if (Str[0] != 0)                        \
+            fprintf(stdout, " (%s)", Str);      \
+        break;                                  \
     }
 #include "token.def"
 #undef X
     if (tok->value)
-        fprintf(stdout, "(%s)", tok->value->buf);
+        fprintf(stdout, "with value: %s", tok->value->buf);
     fprintf(stdout, "\n");
 }
