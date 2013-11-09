38db4bbc2db98b40cc0cd2ac1a2e61bbe61737f5
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-10 00:56:07 +0100

    utils: add logging macro (LOG)

---
+++ src/utils/log.h
@@ -0,0 +1,36 @@
+#ifndef LOG_H
+# define LOG_H
+
+enum log_level
+{
+    INFO,
+    WARN,
+    ERROR
+};
+typedef enum log_level e_log_level;
+
+struct debug_info
+{
+    char *file;
+    int line;
+};
+typedef struct debug_info s_debug_info;
+
+# define COLOR_BLUE    "\x1b[34m"
+# define COLOR_RED     "\x1b[31m"
+# define COLOR_YELLOW  "\x1b[33m"
+# define COLOR_RESET   "\x1b[0m"
+
+# define LOG(Level, fmt, ...)                     \
+    do {                                          \
+        s_debug_info debug =                      \
+        {                                         \
+            .file = __FILE__,                     \
+            .line = __LINE__                      \
+        };                                        \
+        my_log(Level, debug, fmt, __VA_ARGS__);   \
+    } while(0)
+
+void my_log(e_log_level level, s_debug_info debug, char *fmt, ...);
+
+#endif
