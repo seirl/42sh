00dad79382b1b929c032e00a2dc957dafaec83f4
Paul Hervot <paul.hervot@epita.fr>
2013-11-17 10:27:26 +0100

    readline: fix ^C and ^D behaviour

--- src/readline/special_keys.c
+++ src/readline/special_keys.c
@@ -5,17 +5,21 @@
 
 static e_next_action handle_special_key(e_special_key key, s_term *term)
 {
-    (void)term;
     switch (key)
     {
         case ENTER:
             return RETURN;
+        case CTRL_C:
+            printf("%s\n", "^C");
             printf("%s", env_get("PS1"));
+            string_reset(term->input);
             fflush(stdout);
             return CONTINUE;
         case CTRL_D:
-            return 2;
-            break;
+            if (term->input->len != 0)
+                return CONTINUE;
+            string_puts(term->input, "exit");
+            return RETURN;
         default:
             return CONTINUE; // Unrecognized key, just ignore it.
     }
