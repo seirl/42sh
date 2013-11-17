1fc82d92b9b964b5ddc659cd4c93e73fcad9ab2d
Rémi Audebert <remi.audebert@epita.fr>
2013-11-17 04:55:40 +0100

    tests: do not display errors twice

--- tests/qdunittest/runner.py
+++ tests/qdunittest/runner.py
@@ -88,7 +88,7 @@ class QDTestRunner:
         result.errors = errors
         result.skipped = skipped
         result.testsRun = tests_run
-        result.print_summary(self.number)
+        result.print_summary(self.number, show_errors=not self.options.select)
 
         return result
 
