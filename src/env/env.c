d60e69ff88ffa2b6de4d371e3896f36d96fef5e0
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-17 04:32:36 +0100

    env: add missing files

---
+++ src/env/env.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "env.h"
+#include "hashtbl.h"
+
+HASHTBL(char*, char*, env);
+
+static s_env *env_get_ptr()
+{
+    static s_env *env_var = NULL;
+    if (env_var == NULL)
+        HASHTBL_INIT(env_var, 100, hash_char, cmp_char, NULL);
+    return env_var;
+}
+
+void env_set(char *name, char *value)
+{
+    HASHTBL_SET(env_get_ptr(), name, value);
+}
+
+char *env_get(char *name)
+{
+    int changed = 0;
+    char *ret;
+    HASHTBL_GET(env_get_ptr(), name, ret, changed);
+    if (changed)
+        return ret;
+    return NULL;
+}
+
+void env_unset(char *name)
+{
+    HASHTBL_DEL(env_get_ptr(), name);
+}
+
+void env_free()
+{
+    HASHTBL_FREE(env_get_ptr());
+}
---
+++ src/env/env.h
@@ -0,0 +1,9 @@
+#ifndef ENV_H
+# define ENV_H
+
+void env_set(char *name, char *value);
+char *env_get(char *name);
+void env_unset(char *name);
+void env_free();
+
+#endif /* !ENV_H */
