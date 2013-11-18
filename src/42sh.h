5e9126af45d04a0d35dc5de34b882c2664443bce
Nassim Eddequiouaq <nassim.eddequiouaq@epita.fr>
2013-11-18 09:52:23 +0100

    shell: add 42sh struct count fields and use s_string*

--- src/42sh.h
+++ src/42sh.h
@@ -1,15 +1,18 @@
 #ifndef 42SH_H
 # define 42SH_H
 
-HASHTBL(char *, s_ast_shell_cmd *, 42sh_funcs, null_free, null_free);
-HASHTBL(char *, char *, 42sh_vars, null_free, null_free);
+HASHTBL(s_string *, s_ast_shell_cmd *, 42sh_funcs, null_free, null_free);
+HASHTBL(s_string *, s_string *, 42sh_vars, null_free, null_free);
 HASHTBL(char *, void *(handler)(), 42sh_builtins, null_free, null_free);
 
 typedef struct 42sh
 {
     s_42sh_funcs *funcs;
+    unsigned int fun_count;
     s_42sh_vars *vars;
+    unsigned int var_count;
     s_42sh_builtins *builtins;
+    unsigned int built_count;
     int status;
 } s_42sh;
 
