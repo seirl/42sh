0420663d37131422c565f3c828e3ab302e2d9fb7
Paul Hervot <paul.hervot@epita.fr>
2013-11-19 00:49:58 +0100

    history: implement readonly history (not tested)

--- src/history/history.c
+++ src/history/history.c
@@ -1,11 +1,44 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "env.h"
 #include "history.h"
 
-static int open_history_file(void)
+static s_history g_hist;
+
+void open_history(void)
 {
     if (!env_get("HISTFILE"))
         env_set("HISTFILE", "~/.sh_history");
+    if (!env_get("HISTFILESIZE"))
+        env_set("HISTFILESIZE", "500");
 
-    FILE *histfile = fopen(env_get("HISTFILE"), "a+");
+    if (access(env_get("HISTFILE"), R_OK))
+        return;
+
+    FILE *hist_file = fopen(env_get("HISTFILE"), "r");
+    // TODO: try to truncate the start of the file if there is more line than
+    // HISTFILESIZE
+    LIST_INIT(g_hist.lines, string_free);
+    char *line = NULL;
+    while (getline(&line, 0, hist_file))
+        LIST_INSERT_HEAD(g_hist.lines, string_create_from(line));
+    g_hist.last_file_entry = g_hist.lines->head;
+}
+
+void close_history(void)
+{
+    // TODO: Write history
+    if (g_hist.lines)
+        LIST_FREE(g_hist.lines);
+}
+
+s_string *query_history(int n)
+{
+    s_Node_hist_lines *elt = NULL;
+    LIST_AT(g_hist.lines, n, elt);
+    return elt->data;
 }
--- src/history/history.h
+++ src/history/history.h
@@ -2,6 +2,15 @@
 # define HISTORY_H
 
 # include "string_utils.h"
+# include "list.h"
+
+LIST(s_string *, hist_lines);
+struct history
+{
+    s_hist_lines *lines;
+    s_Node_hist_lines *last_file_entry;
+};
+typedef struct history s_history;
 
 /**
 ** @brief Query the command line history to get nth last line.
@@ -9,6 +18,8 @@
 ** @param n The number from the end of the wanted command line.
 ** @return an s_string representing the wanted command line.
 */
-s_string *get_history(size_t n);
+s_string *get_history(int n);
+void open_history(void);
+void close_history(void);
 
 #endif /* !HISTORY_H */
