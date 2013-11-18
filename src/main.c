29f5eabab2d3231c8c5c45be9ad7881046f32598
Paul Hervot <paul.hervot@epita.fr>
2013-11-18 04:53:58 +0100

    main: setup lexer and parser (not tested)

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "options.h"
+#include "lexer.h"
+#include "parser.h"
 #include "input.h"
 
 int main(int argc, char *argv[])
@@ -7,5 +9,9 @@ int main(int argc, char *argv[])
     int ret = parse_options(argc, argv);
     if (ret)
         return ret;
+
+    s_lexer *lexer = lex_create(input_getc, input_topc, "<stdin>");
+    s_parser *parser = parser_create(lexer);
+    (void)parser;
     return EXIT_SUCCESS;
 }
