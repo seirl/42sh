9e3b161bdbf85059c73626d13b7ab30036c084d9
Paul Hervot <paul.hervot@epita.fr>
2013-11-20 00:28:31 +0100

    readline: directly use the global value when initializing the terminal

--- src/readline/terminal.c
+++ src/readline/terminal.c
@@ -7,36 +7,35 @@
 
 static s_term *g_term = NULL;
 
-static s_term *term_init(void)
+static void term_init(void)
 {
-    s_term *term = smalloc(sizeof (struct terminal));
-    if (tcgetattr(STDIN_FILENO, &term->restore_state) != 0)
+    g_term = smalloc(sizeof (struct terminal));
+    if (tcgetattr(STDIN_FILENO, &g_term->restore_state) != 0)
     {
-        sfree(term);
-        return NULL;
+        sfree(g_term);
+        g_term = NULL;
     }
-    term->termios = term->restore_state;
-    term->name = env_get("TERM");
-    term->bp = smalloc(2048);
-    if (tgetent(term->bp, term->name) <= 0)
-        tgetent(term->bp, "xterm");
-    term->termios.c_iflag &= ~ICRNL;
-    term->termios.c_lflag &= ~(ICANON | ECHO | ISIG);
-    term->termios.c_cc[VMIN] = 1;
-    term->termios.c_cc[VTIME] = 0;
-    tcsetattr(STDIN_FILENO, TCSADRAIN, &term->termios);
-    term->input = string_create(0);
-    term->input_index = 0;
-    term->hist_current = NULL;
-    term->hist_pos = 0;
-    term->prompt = NULL;
-    return term;
+    g_term->termios = g_term->restore_state;
+    g_term->name = env_get("TERM");
+    g_term->bp = smalloc(2048);
+    if (tgetent(g_term->bp, g_term->name) <= 0)
+        tgetent(g_term->bp, "xterm");
+    g_term->termios.c_iflag &= ~ICRNL;
+    g_term->termios.c_lflag &= ~(ICANON | ECHO | ISIG);
+    g_term->termios.c_cc[VMIN] = 1;
+    g_term->termios.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSADRAIN, &g_term->termios);
+    g_term->input = string_create(0);
+    g_term->input_index = 0;
+    g_term->hist_current = NULL;
+    g_term->hist_pos = 0;
+    g_term->prompt = NULL;
 }
 
 s_term *term_get(void)
 {
-    if (g_term == NULL)
-        g_term = term_init();
+    if (!g_term)
+        term_init();
 
     return g_term;
 }
