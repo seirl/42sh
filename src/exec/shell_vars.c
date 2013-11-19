ea2533a8940751489e4f8e285a3256043adbb2ea
Nassim Eddequiouaq <nassim.eddequiouaq@epita.fr>
2013-11-19 23:00:40 +0100

    exec: conflict solved

--- src/exec/shell_vars.c
+++ src/exec/shell_vars.c
@@ -17,33 +17,16 @@ void vars_set(s_string *name, s_string *value)
 
 s_string *vars_get(s_string *name)
 {
-<<<<<<< HEAD
     return hashtbl_get(vars_get_ptr(0), name);
-=======
-    if (!vars_get_ptr())
-        return NULL;
-    return hashtbl_get(vars_get_ptr(), name);
->>>>>>> exec: add protection on hashtbls functions
 }
 
 void vars_unset(s_string *name)
 {
-<<<<<<< HEAD
     hashtbl_unset(vars_get_ptr(0), name);
-=======
-    if (!vars_get_ptr())
-        return;
-    hashtbl_unset(vars_get_ptr(), name);
->>>>>>> exec: add protection on hashtbls functions
 }
 
 void vars_free(void)
 {
-<<<<<<< HEAD
     hashtbl_free(vars_get_ptr(0));
     vars_get_ptr(1);
-=======
-    if (vars_get_ptr())
-        hashtbl_free(vars_get_ptr());
->>>>>>> exec: add protection on hashtbls functions
 }
