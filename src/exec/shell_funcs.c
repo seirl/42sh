555081f0bac7692a2004e9e1cb24ff9c1b729349
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-19 23:18:54 +0100

    exec: fix segfault

--- src/exec/shell_funcs.c
+++ src/exec/shell_funcs.c
@@ -1,29 +1,32 @@
 #include "shell_funcs.h"
 
-static s_hashtbl *funcs_get_ptr(void)
+static s_hashtbl *funcs_get_ptr(int reset)
 {
     static s_hashtbl *funcs = NULL;
-    if (funcs == NULL)
+    if (reset)
+        funcs = NULL;
+    else if (funcs == NULL)
         funcs = hashtbl_init(hash_char, cmp_char, free_char, free_char);
     return funcs;
 }
 
 void funcs_set(char *name, s_ast_shell_cmd *value)
 {
-    hashtbl_set(funcs_get_ptr(), name, value);
+    hashtbl_set(funcs_get_ptr(0), name, value);
 }
 
 s_ast_shell_cmd *funcs_get(char *name)
 {
-    return hashtbl_get(funcs_get_ptr(), name);
+    return hashtbl_get(funcs_get_ptr(0), name);
 }
 
 void funcs_unset(char *name)
 {
-    hashtbl_unset(funcs_get_ptr(), name);
+    hashtbl_unset(funcs_get_ptr(0), name);
 }
 
 void funcs_free(void)
 {
-    hashtbl_free(funcs_get_ptr());
+    hashtbl_free(funcs_get_ptr(0));
+    funcs_get_ptr(1);
 }
--- src/exec/shell_vars.c
+++ src/exec/shell_vars.c
@@ -1,29 +1,32 @@
 #include "shell_vars.h"
 
-static s_hashtbl *vars_get_ptr(void)
+static s_hashtbl *vars_get_ptr(int reset)
 {
     static s_hashtbl *vars = NULL;
-    if (vars == NULL)
+    if (reset)
+        vars = NULL;
+    else if (vars == NULL)
         vars = hashtbl_init(hash_string, cmp_string, free_string, free_string);
     return vars;
 }
 
 void vars_set(s_string *name, s_string *value)
 {
-    hashtbl_set(vars_get_ptr(), name, value);
+    hashtbl_set(vars_get_ptr(0), name, value);
 }
 
 s_string *vars_get(s_string *name)
 {
-    return hashtbl_get(vars_get_ptr(), name);
+    return hashtbl_get(vars_get_ptr(0), name);
 }
 
 void vars_unset(s_string *name)
 {
-    hashtbl_unset(vars_get_ptr(), name);
+    hashtbl_unset(vars_get_ptr(0), name);
 }
 
 void vars_free(void)
 {
-    hashtbl_free(vars_get_ptr());
+    hashtbl_free(vars_get_ptr(0));
+    vars_get_ptr(1);
 }
