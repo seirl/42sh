945b0eba02dac98ae465315b145291947cb62074
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-19 05:42:05 +0100

    input: detect interactive mode

--- src/input/input.c
+++ src/input/input.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "string_utils.h"
@@ -6,8 +7,9 @@
 #include "input_string.h"
 #include "input_file.h"
 #include "lexer_private.h"
+#include "readline.h"
 
-s_lexer *input_to_lexer(char *cmd, char *file)
+s_lexer *input_to_lexer(char *cmd, char *file, int *repeat)
 {
     s_string *input;
     if (cmd)
@@ -20,7 +22,19 @@ s_lexer *input_to_lexer(char *cmd, char *file)
         FILE *f = fopen(file, "r");
         return lex_create(input_file_getc, input_file_topc, f, "file");
     }
-    return NULL;
+    *repeat = isatty(STDIN_FILENO);
+    if (!*repeat)
+    {
+        //FIXME STDIN
+        return NULL;
+    }
+    input = readline("42sh> ");
+    if (input == NULL || !strcmp(input->buf, "exit"))
+    {
+        *repeat = 0;
+        return NULL;
+    }
+    return lex_create(input_string_getc, input_string_topc, input, "int");
 }
 
 void input_free(s_lexer *lexer, char *cmd, char *file)
--- src/main.c
+++ src/main.c
@@ -10,12 +10,17 @@ int main(int argc, char *argv[])
 {
     char *file = NULL;
     char *cmd = NULL;
+    int repeat = 0;
     int ret = parse_options(argc, argv, &cmd, &file);
     if (ret)
         return ret;
-    s_lexer *lexer = input_to_lexer(cmd, file);
-    s_parser *parser = parser_create(lexer);
-    (void)parser;
-    input_free(lexer, cmd, file);
+    do {
+        s_lexer *lexer = input_to_lexer(cmd, file, &repeat);
+        if (lexer == NULL)
+            break;
+        s_parser *parser = parser_create(lexer);
+        (void)parser;
+        input_free(lexer, cmd, file);
+    } while (repeat);
     return EXIT_SUCCESS;
 }
