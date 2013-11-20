b7d66f9d5ba46db577e65e9a75b6d7109838e6e4
Paul Hervot <paul.hervot@epita.fr>
2013-11-20 16:46:13 +0100

    readline: do not use a global variable for terminal datas

--- src/readline/terminal.c
+++ src/readline/terminal.c
@@ -5,49 +5,57 @@
 #include "env.h"
 #include "smalloc.h"
 
-static s_term *g_term = NULL;
+static s_term *get_term_ptr(int set, s_term *new_term)
+{
+    static s_term *term = NULL;
+    if (set)
+        term = new_term;
+    return term;
+}
 
 static void term_init(void)
 {
-    g_term = smalloc(sizeof (struct terminal));
-    if (tcgetattr(STDIN_FILENO, &g_term->restore_state) != 0)
+    get_term_ptr(1, smalloc(sizeof (struct terminal)));
+    s_term *term = get_term_ptr(0, NULL);
+    if (tcgetattr(STDIN_FILENO, &term->restore_state) != 0)
     {
-        sfree(g_term);
-        g_term = NULL;
+        sfree(term);
+        term = NULL;
     }
-    g_term->termios = g_term->restore_state;
-    g_term->name = env_get("TERM");
-    g_term->bp = smalloc(2048);
-    if (tgetent(g_term->bp, g_term->name) <= 0)
-        tgetent(g_term->bp, "xterm");
-    g_term->termios.c_iflag &= ~ICRNL;
-    g_term->termios.c_lflag &= ~(ICANON | ECHO | ISIG);
-    g_term->termios.c_cc[VMIN] = 1;
-    g_term->termios.c_cc[VTIME] = 0;
-    tcsetattr(STDIN_FILENO, TCSADRAIN, &g_term->termios);
-    g_term->backup_input = string_create(0);
-    g_term->input = g_term->backup_input;
-    g_term->input_index = 0;
-    g_term->hist_current = NULL;
-    g_term->hist_pos = -1;
-    g_term->prompt = NULL;
+    term->termios = term->restore_state;
+    term->name = env_get("TERM");
+    term->bp = smalloc(2048);
+    if (tgetent(term->bp, term->name) <= 0)
+        tgetent(term->bp, "xterm");
+    term->termios.c_iflag &= ~ICRNL;
+    term->termios.c_lflag &= ~(ICANON | ECHO | ISIG);
+    term->termios.c_cc[VMIN] = 1;
+    term->termios.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSADRAIN, &term->termios);
+    term->backup_input = string_create(0);
+    term->input = term->backup_input;
+    term->input_index = 0;
+    term->hist_current = NULL;
+    term->hist_pos = -1;
+    term->prompt = NULL;
 }
 
 s_term *term_get(void)
 {
-    if (!g_term)
+    if (!get_term_ptr(0, NULL))
         term_init();
 
-    return g_term;
+    return get_term_ptr(0, NULL);
 }
 
 void term_close(void)
 {
-    if (!g_term)
+    s_term *term = get_term_ptr(0, NULL);
+    if (!term)
         return;
-    sfree(g_term->bp);
-    string_free(g_term->backup_input);
-    tcsetattr(STDIN_FILENO, TCSANOW, &(g_term->restore_state));
-    sfree(g_term);
-    g_term = NULL;
+    sfree(term->bp);
+    string_free(term->backup_input);
+    tcsetattr(STDIN_FILENO, TCSANOW, &(term->restore_state));
+    sfree(term);
+    get_term_ptr(1, NULL);
 }
