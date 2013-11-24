35ffa2924cb2b2b5f1e1f945393e63125c91e2af
Antoine Pietri <antoine.pietri@epita.fr>
2013-11-24 19:18:50 +0100

    expand: handle #

--- src/expand/expand_substs_var.c
+++ src/expand/expand_substs_var.c
@@ -1,15 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "string_utils.h"
 #include "ast.h"
 #include "shell.h"
 #include "expand.h"
+#include "env.h"
 
 s_string *expand_substs_var(const s_shell *shell, const s_string *word)
 {
-    (void) shell;
-    (void) word;
-    // TODO
-
     if (word->buf[0] == '#')
-        return string_itoa(0);
+    {
+        const char *r = env_get(shell, word->buf + 1);
+        return string_itoa(r ? strlen(r) : 0);
+    }
+
+    s_string *varname = string_create(0);
+    for (unsigned i = 0; word->buf[i] != '\0' && word->buf[i] != ':'; i++)
+        string_putc(varname, word->buf[i]);
+    s_string *var_content = string_create_from(env_get(shell, varname->buf));
+    free(varname);
+    (void) var_content;
+
     return NULL;
 }
