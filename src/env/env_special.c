76354afb625717f763bc1e84b976ea7bfc6b8d61
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-24 18:42:59 +0100

    env: $UID

--- src/env/env_special.c
+++ src/env/env_special.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <time.h>
 
@@ -11,9 +12,18 @@ static char *env_random(void)
     return strdup(ret);
 }
 
+static char *env_uid(void)
+{
+    char buf[32];
+    snprintf(buf, 32, "%d", getuid());
+    return strdup(buf);
+}
+
 char *env_special(char *name)
 {
     if (!strcmp(name, "RANDOM"))
         return env_random();
+    if (!strcmp(name, "UID"))
+        return env_uid();
     return NULL;
 }
