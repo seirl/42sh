e1093e9f64abed6d0708b6af8b452b2de3fcacf8
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-24 17:59:35 +0100

    builtins: simple set builtin

---
+++ src/builtins/b_set.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "builtins.h"
+#include "shell_private.h"
+#include "macros.h"
+#include "hashtbl.h"
+#include "env_private.h"
+
+
+int builtin_set(s_shell *shell, int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    s_hash_elt *it = NULL;
+    void *key;
+    char *char_key;
+    s_env_var *value;
+    while ((value = hashtbl_iter(shell->env, &key, &it)) != NULL)
+    {
+        char_key = key;
+        printf("%s=%s\n", char_key, value->value);
+    }
+    return 0;
+}
