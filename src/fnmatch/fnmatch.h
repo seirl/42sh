d49229c322b629fea8db0cd1e00c6c35563d92a2
Antoine Pietri <antoine.pietri@epita.fr>
2013-11-25 04:59:33 +0100

    fnmatch: add glob declaration

--- src/fnmatch/fnmatch.h
+++ src/fnmatch/fnmatch.h
@@ -1,6 +1,7 @@
 #ifndef MY_FNMATCH_H
 # define MY_FNMATCH_H
 
-int my_fnmatch(const char *p, const char *str);
+int my_fnmatch(const char *pattern, const char *str);
+char **my_glob(const char *pattern, int *count);
 
 #endif /* !MY_FNMATCH_H */
