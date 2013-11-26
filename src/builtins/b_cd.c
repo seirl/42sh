3cc05460acc46a72f1f02d6d19192e7af881e790
Nassim Eddequiouaq <nassim.eddequiouaq@epita.fr>
2013-11-26 15:58:42 +0100

    builtins: fix cd

--- src/builtins/b_cd.c
+++ src/builtins/b_cd.c
@@ -6,7 +6,6 @@
 #include "builtins.h"
 #include "macros.h"
 #include "string.h"
-#include "smalloc.h"
 #include "log.h"
 #include "shell_private.h"
 #include "shell.h"
@@ -16,51 +15,20 @@
 # define _GNU_SOURCE
 #endif
 
-static char *path_concat(char *dir)
-{
-    if (dir && (dir[0] == '/'))
-        return dir;
-
-    char *path = getenv("HOME");
-    /* final char* : "$HOME/DIR\0" */
-    int len_path = strlen(path);
-    int len_dir = strlen(dir);
-    char *res = smalloc(len_path + len_dir + 2);
-    int i = 0;
-    for (; i < len_path; ++i)
-        res[i] = path[i];
-    res[len_path] = '/';
-    i += 1;
-    for (; i < len_dir + 1 + len_path; ++i)
-        res[i] = dir[i - len_path - 1];
-    res[len_path + len_dir + 1] = '\0';
-    return res;
-}
-
 static int cd_to_dir(s_shell *shell, char *dir)
 {
-    char *pwd = NULL;
-    if (!strcmp(dir, "."))
-    {
-        pwd = getenv("PWD");
-        setenv("OLDPWD", pwd, 1);
-        env_set(shell, pwd, "OLDPWD");
-        return 0;
-    }
-
+    char *curr_dir = get_current_dir_name();
+    char *new_path = NULL;
     if (chdir(dir))
     {
         fprintf(stderr, "42sh: cd: %s: No such file or directory\n", dir);
         return 1;
     }
-    if ((pwd = getenv("PWD")))
-    {
-        setenv("OLDPWD", pwd, 1);
-        env_set(shell, pwd, "OLDPWD");
-        char *complete_path = path_concat(dir);
-        setenv("PWD", complete_path, 1);
-        env_set(shell, complete_path, "PWD");
-    }
+    setenv("OLDPWD", curr_dir, 1);
+    env_set(shell, curr_dir, "OLDPWD");
+    new_path = get_current_dir_name();
+    setenv("PWD", new_path, 1);
+    env_set(shell, new_path, "PWD");
     return 0;
 }
 
