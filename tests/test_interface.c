c473a35a455581927ffcb52807af11a4b9723515
Rémi Audebert <remi.audebert@epita.fr>
2013-11-26 12:42:24 +0100

    interface: fix leak in test

--- tests/test_interface.c
+++ tests/test_interface.c
@@ -1,16 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 #include "options.h"
+#include "smalloc.h"
 #include "shell.h"
 
 int main(int argc, char *argv[])
 {
     char *src;
     s_shell *shell = shell_new();
+
     int ret = parse_options(shell, argc, argv, &src);
+
     shell_delete(shell);
+    smalloc_clean();
+
     if (ret & E_ERROR)
         return ret;
-
     return EXIT_SUCCESS;
 }
