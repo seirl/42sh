23065b6d51e5333a716796381812cff884140d48
Nassim Eddequiouaq <nassim.eddequiouaq@epita.fr>
2013-11-26 16:56:57 +0100

    builtins: fix cd leaks and invalid read

--- src/builtins/b_cd.c
+++ src/builtins/b_cd.c
@@ -20,18 +20,18 @@
 
 static int cd_to_dir(s_shell *shell, char *dir)
 {
-    char *curr_dir = smalloc(SHELL_PATH_MAX);
+    char *curr_dir = scalloc(SHELL_PATH_MAX, sizeof (char));
     curr_dir = getcwd(curr_dir, SHELL_PATH_MAX);
     char *new_path = NULL;
     if (chdir(dir))
     {
-        sfree(dir);
         fprintf(stderr, "42sh: cd: %s: No such file or directory\n", dir);
+        sfree(dir);
         return 1;
     }
     setenv("OLDPWD", curr_dir, 1);
     env_set(shell, curr_dir, "OLDPWD");
-    new_path = smalloc(SHELL_PATH_MAX);
+    new_path = scalloc(SHELL_PATH_MAX, sizeof (char));
     new_path = getcwd(new_path, SHELL_PATH_MAX);
     setenv("PWD", new_path, 1);
     env_set(shell, new_path, "PWD");
