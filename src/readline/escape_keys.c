46895534b04b528e691a52f94019a20b1e9704e0
Paul Hervot <paul.hervot@epita.fr>
2013-11-26 20:41:01 +0100

    readline: handle alt+.

--- src/readline/escape_keys.c
+++ src/readline/escape_keys.c
@@ -4,6 +4,8 @@
 #include "bracket_key.h"
 #include "escape_keys.h"
 #include "terminal.h"
+#include "readline.h"
+#include "wrapper.h"
 
 static e_next_action do_b(s_shell *shell, s_term *term)
 {
@@ -23,6 +25,35 @@ static e_next_action do_f(s_shell *shell, s_term *term)
         handle_bracket_key(shell, BRACKET_RIGHT, term);
     while (*i < len && !input_is_blank(term, *i))
         handle_bracket_key(shell, BRACKET_RIGHT, term);
+    return CONTINUE;
+}
+
+static e_next_action do_dot(s_shell *shell, s_term *term)
+{
+    s_string *last_line = string_duplicate(history_get(shell, 0)->line);
+    size_t end = last_line->len;
+    size_t i = end;
+    while (i && (last_line->buf[i] == ' ' || last_line->buf[i] == '\t'))
+        i--;
+    while (i && last_line->buf[i] != ' ' && last_line->buf[i] != '\t')
+        i--;
+
+    if (last_line->buf[i] == ' ' || last_line->buf[i] == '\t')
+        i++;
+
+    my_tputs(tgetstr("im", NULL));
+    while (i < end)
+    {
+        string_insertc(term->input, last_line->buf[i], term->input_index);
+        term->input_index++;
+
+        printf("%c", last_line->buf[i]);
+        fflush(stdout);
+        i++;
+    }
+    my_tputs(tgetstr("ei", NULL));
+
+    string_free(last_line);
     return CONTINUE;
 }
 
