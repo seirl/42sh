2bcdf490efc21cfe796bf6c849530b3e05e09975
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-27 23:45:44 +0100

    expand: code cleaning expand wordlist

--- src/expand/expand_wordlist.c
+++ src/expand/expand_wordlist.c
@@ -25,52 +25,51 @@ static s_ast_compound_word *cw_add_word(s_ast_compound_word *cw, s_string *s)
     return cw;
 }
 
-static s_ast_compound_word *split_word(s_shell *shell, s_ast_word *word,
-        s_ast_compound_word *cw)
+static int replace_current_word(s_shell *shell, char *value, s_ast_word *word)
 {
-    if (word->kind != EXPAND_NAME)
-        return NULL;
-
-    char *value = env_get(shell, word->str->buf);
-    //printf("ENV get |%s|\n", value);
-
-    //replace current word
     string_reset(word->str);
     int i;
     for (i = 0; value[i] && !is_ifs(shell, value[i]); ++i)
         string_putc(word->str, value[i]);
     word->kind = WORD;
-    //printf("Word is now %s\n", word->str->buf);
     for (; value[i] && is_ifs(shell, value[i]); ++i)
         continue;
-    if (value[i] == 0) //variable without split
+    return i;
+}
+
+static s_ast_compound_word *split_word(s_shell *shell, s_ast_word *word,
+                                       s_ast_compound_word *cw)
+{
+    if (word->kind != EXPAND_NAME)
+        return NULL;
+    char *value = env_get(shell, word->str->buf);
+
+    int i = replace_current_word(shell, value, word);
+    if (value[i] == 0)
         return NULL;
     --i;
 
-    //fill the garbage words list
     s_string *buf = string_create(0);
     ++i;
     for (; value[i]; ++i)
     {
         if (is_ifs(shell, value[i]))
         {
-            //printf("Add %s\n", buf->buf);
             cw = cw_add_word(cw, buf);
             string_reset(buf);
             continue;
         }
         string_putc(buf, value[i]);
     }
-    //printf("Add %s\n", buf->buf);
     cw = cw_add_word(cw, buf);
     string_free(buf);
     return cw;
 }
 
-static s_ast_compound_word *split_compound_word(s_shell *shell, s_ast_compound_word *cw)
+static s_ast_compound_word *split_compound_word(s_shell *shell,
+                                                s_ast_compound_word *cw)
 {
-    //printf("Split compound\n");
-    //TODO merge last
+    //TODO: schischi merge last
     s_ast_compound_word *it;
     s_ast_compound_word *res = NULL;
     for (it = cw; it; it = it->next)
@@ -86,7 +85,6 @@ static void add_wl(s_ast_word_list *l, s_ast_compound_word *cw)
     s_ast_word_list *prev = NULL;
     while (it)
     {
-        //printf("Garbage %s\n", it->word->str->buf);
         nl = smalloc(sizeof (s_ast_word_list));
         nl->word = smalloc(sizeof (s_ast_compound_word));
         nl->word->next = NULL;
@@ -110,10 +108,8 @@ static void add_wl(s_ast_word_list *l, s_ast_compound_word *cw)
 
 void expand_wordlist(s_shell *shell, s_ast_word_list *elt)
 {
-    //printf("Expand wordlist\n");
     s_ast_word_list *wl;
     s_ast_compound_word *cw = NULL;
-    //s_ast_compound_word *it = NULL;
     for (wl = elt; wl; wl = wl->next)
     {
         cw = split_compound_word(shell, wl->word);
