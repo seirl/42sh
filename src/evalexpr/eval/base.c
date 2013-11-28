d59d4580544909cea44cb525b04863a79bf6f2f7
Antoine Pietri <antoine.pietri@epita.fr>
2013-11-28 23:39:27 +0100

    evalexpr: div by 0 no longer SIGFPE

--- src/evalexpr/eval/base.c
+++ src/evalexpr/eval/base.c
@@ -1,3 +1,5 @@
+#include "log.h"
+
 long op_add(long a, long b)
 {
     return a + b;
@@ -15,10 +17,20 @@ long op_mul(long a, long b)
 
 long op_div(long a, long b)
 {
+    if (!b)
+    {
+        LOG(ERROR, "Division by 0 (%d/%d)", a, b);
+        return 0;
+    }
     return a / b;
 }
 
 long op_mod(long a, long b)
 {
+zsh:1: command not found: :x
+    {
+        LOG(ERROR, "Division by 0 (%d/%d)", a, b);
+        return 0;
+    }
     return a % b;
 }
