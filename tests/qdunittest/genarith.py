b5a87b94ec669231c1554cdb49bfcede7b3b619e
Antoine Pietri <pietri_a@epita.fr>
2013-11-30 00:53:05 +0100

    tests: add genarith.py

---
+++ tests/qdunittest/genarith.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import random
+import subprocess
+
+binary = ['+', '-', '*']
+unary = ['+', '-']
+
+def gen_arithm(depth):
+    if not random.randint(0, depth):
+        return str(random.randint(1, 1000))
+    if not random.randint(0, 4):
+        return '({})'.format(gen_arithm(depth - 1))
+    if not random.randint(0, 20):
+        return '{}{}'.format(random.choice(unary), gen_arithm(depth - 1))
+    return '{} {} {}'.format(gen_arithm(depth - 1), random.choice(binary),
+                                 gen_arithm(depth - 1))
+
+def gen_arithm_test():
+    random.seed()
+    i = gen_arithm(10)
+    r = subprocess.check_output(["echo $(( {} ))".format(i)], shell=True)
+    r = int(r.strip())
+
+    return {
+        "desc": "Random arithmetic",
+        "input": "echo $(( {} ))".format(i),
+        "output": '{}'.format(r),
+    }
