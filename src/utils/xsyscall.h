6c0279f7eb0afcd8a6c96d87458d8447b3aaf5cb
Rémi Audebert <remi.audebert@epita.fr>
2013-11-30 23:43:17 +0100

    utils: add XPIPE

--- src/utils/xsyscall.h
+++ src/utils/xsyscall.h
@@ -13,4 +13,18 @@
             LOG(ERROR, "close: %d", Fd);    \
     } while (0)
 
+# define XPIPE(Fds)                          \
+    do {                                     \
+        int _ret = pipe(Fds);                \
+        if (_ret < 0)                        \
+            LOG(ERROR, "pipe failed", NULL); \
+    } while (0)
+
+# define XWRITE(Fd, Str, Len)                 \
+    do {                                      \
+        int _ret = write(Fd, Str, Len);       \
+        if (_ret < 0)                         \
+            LOG(ERROR, "write failed", NULL); \
+    } while (0)
+
 #endif /* !XSYSCALL_H */
