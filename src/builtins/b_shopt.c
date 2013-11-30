ccbc87999bb333a07df118c46c111e5d4bab05dd
Adrien Schildknecht <adrien+dev@schischi.me>
2013-11-30 23:33:05 +0100

    builtin: shopt, add -q

--- src/builtins/b_shopt.c
+++ src/builtins/b_shopt.c
@@ -38,7 +38,7 @@ static int foreach_opt(s_shell *shell, s_opt *opt, int value)
     return 0;
 }
 
-static int print_shopt_argv(s_shell *shell, s_opt *opt)
+static int print_shopt_argv(s_shell *shell, s_opt *opt, int print)
 {
     int ret = 0;
     for (unsigned int i = 0; i < opt->trailing_count; ++i)
@@ -51,7 +51,8 @@ static int print_shopt_argv(s_shell *shell, s_opt *opt)
                     "invalid shell option name\n", opt_trailing_arg(opt, i));
             return 1;
         }
-        printf("%s\t%s\n", name, shopt_value == 1 ? "on" : "off");
+        if (print)
+            printf("%s\t%s\n", name, shopt_value == 1 ? "on" : "off");
         ret |= !shopt_value;
     }
     return ret;
@@ -60,20 +61,23 @@ static int print_shopt_argv(s_shell *shell, s_opt *opt)
 int builtin_shopt(s_shell *shell, int argc, char *argv[])
 {
     int ret = 0;
+    int print = 1;
     s_opt *opt = opt_init(shopt_param, 3);
     if (opt_parse(argc, argv, opt))
     {
         usage();
         return 2;
     }
-    if (opt->trailing_count == 0)
+    if (opt_get(opt, "q", NULL))
+        print = 0;
+    if (opt->trailing_count == 0 && print)
         shopt_print(shell);
     else if (opt_get(opt, "s", NULL))
         ret = foreach_opt(shell, opt, 1);
     else if (opt_get(opt, "u", NULL))
         ret = foreach_opt(shell, opt, 0);
     else
-        ret = print_shopt_argv(shell, opt);
+        ret = print_shopt_argv(shell, opt, print);
     opt_free(opt);
     return ret;
 }
