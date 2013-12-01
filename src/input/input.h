fc6fe5c362c01f01ad18b157b1b4b866b9f0da6c
Adrien Schildknecht <adrien+dev@schischi.me>
2013-12-01 11:34:32 +0100

    input: doxygen for the resource loader

--- src/input/input.h
+++ src/input/input.h
@@ -10,7 +10,9 @@ struct shell;
 
 typedef struct input s_input;
 
+/** @brief Create an input from a string or a file */
 s_input *input_create(struct shell *shell, char *src, int mode);
+/** @brief Free the resource of the input */
 void input_destroy(s_input *input);
 
 #endif /* !INPUT_H */
--- src/input/res_ctrl.h
+++ src/input/res_ctrl.h
@@ -3,6 +3,7 @@
 
 # include "shell.h"
 
+/** @brief Load the rc files */
 int rc_file_load(s_shell *shell);
 
 #endif /* !RES_CTRL_H */
