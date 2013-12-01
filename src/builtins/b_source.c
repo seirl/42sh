e775ab25f4db99feaa9de12fee0143c7e2226c8c
Nassim Eddequiouaq <nassim.eddequiouaq@epita.fr>
2013-12-01 11:14:13 +0100

    builtins: fix retvalue error in source builtin

--- src/builtins/b_source.c
+++ src/builtins/b_source.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "builtins.h"
 #include "shell_private.h"
 #include "macros.h"
@@ -10,15 +11,17 @@
 int builtin_source(s_shell *shell, int argc, char **argv)
 {
     FILE *f = NULL;
-    if (argc < 2)
+    if (argc < 2 || ((argc == 2) && (!argv[1] || (!strcmp(argv[1], "")))))
     {
         LOG(WARN, "42sh: %s: filename argument requires\n", argv[0]);
         LOG(WARN, "%s: usage: source filename [arguments]\n", argv[0]);
+        shell_status_set(shell, 2);
         return 2;
     }
     if (!(f = fopen(argv[1], "r")))
     {
         LOG(WARN, "42sh: source: %s: file not found\n", argv[1]);
+        shell_status_set(shell, 1);
         return 1;
     }
     s_parser *parser = shell->parser;
