c3db87c39b6b63e82bd5da4c1e370075f0fb427b
Rémi Audebert <remi.audebert@epita.fr>
2013-12-01 06:34:27 +0100

    exec: fix leak in assignment

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -21,14 +21,16 @@ void exec_assignment(s_shell *shell, s_ast_assignment *assign)
 {
     s_string *name = string_duplicate(assign->name);
     if (!assign->name)
-        env_set(shell, "", string_release(name));
+        env_set(shell, "", name->buf);
     else
     {
         s_string *value;
         if (assign->value)
-            value = string_duplicate(expand_compound(shell, assign->value));
+            value = expand_compound(shell, assign->value);
         else
             value = string_create_from("");
-        env_set(shell, string_release(value), string_release(name));
+        env_set(shell, value->buf, name->buf);
+        string_free(value);
     }
+    string_free(name);
 }
